dbd403e6f6f138c23f32cfb426ce6bd92ee3b7df
Milan Gonzalez-Thauvin <milan.gonzalez-thauvin@ens-lyon.fr>
2023-05-17 21:47:01 +0200

    Update bibparse.py to make it cleaner

--- Makefile
+++ Makefile
@@ -8,7 +8,6 @@ setup:
 menu.txt:
 	sed 's/""/" "/g' cryptobib/abbrev0.bib > abbrev0_no_empty_strings.bib
 	cat abbrev0_no_empty_strings.bib cryptobib/crypto.bib > input.bib
-	@echo "Parsing the big TeX file, can take a while ..."
 	python3 bibparse.py > menu.txt
 
 init: menu.txt
--- bibparse.py
+++ bibparse.py
@@ -1,23 +1,29 @@
 import bibtexparser
 
-with open('input.bib') as bibtex_file:
-    bib_database = bibtexparser.load(bibtex_file)
+def main():
+    print("Parsing the big BibTeX file, can take a while (10 to 15 minutes), it's time for coffee!")
 
-entry_list = bib_database.entries
+    with open('input.bib') as bibtex_file:
+        bib_database = bibtexparser.load(bibtex_file)
 
-entry_list.sort(key=lambda entry:
-    (-int(entry["year"]) if "year" in entry else 0,
-    entry["author"] if "author" in entry else "ZZZ",
-    entry["title"] if "title" in entry else "ZZZ",
-    entry["ID"].replace("EPRINT", "ZEPRINT") if "ID" in entry else "ZZZ"))
-# Sort by year (more recent first) then journal ID and authors.
-# If ambiguity, put EPRINT entries last.
+    entry_list = bib_database.entries
 
-for entry in entry_list:
-    print(entry["ID"] if "ID" in entry else "NO ID", end="; ")
-    print(entry["author"].replace("\n", " ") if "author" in entry else "NO AUTHOR", end="; ")
-    print(entry["title"] if "title" in entry else "NO TITLE", end="")
-    if entry["ID"][:6] == "EPRINT" and "note" in entry and entry["note"][:5] == "\\url{":
-        url = entry["note"][5:-1]  # grab the url of the eprint
-        print(" %", url, end="")
-    print()  # New line
+    entry_list.sort(key=lambda entry:
+        (-int(entry["year"]) if "year" in entry else 0,
+        entry["author"] if "author" in entry else "ZZZ",
+        entry["title"] if "title" in entry else "ZZZ",
+        entry["ID"].replace("EPRINT", "ZZZ") if "ID" in entry else "ZZZ"))
+    # Sort by year (more recent first) then author and title.
+    # If ambiguity, put EPRINT entries last.
+
+    for entry in entry_list:
+        print(entry["ID"] if "ID" in entry else "NO ID", end="; ")
+        print(entry["author"].replace("\n", " ") if "author" in entry else "NO AUTHOR", end="; ")
+        print(entry["title"] if "title" in entry else "NO TITLE", end="")
+        if entry["ID"][:6] == "EPRINT" and "note" in entry and entry["note"][:5] == "\\url{":
+            url = entry["note"][5:-1]  # grab the url of the eprint
+            print(" %", url, end="")
+        print()  # New line
+
+if __name__ == "__main__":
+    main()
