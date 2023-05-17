9228f62c43cf41fe5ab90066fb130712a2e22f2a
Milan Gonzalez-Thauvin <milan.gonzalez-thauvin@ens-lyon.fr>
2023-05-17 16:14:17 +0200

    Sort menu.txt in a relevant way for more efficient research

--- bibparse.py
+++ bibparse.py
@@ -3,12 +3,21 @@ import bibtexparser
 with open('input.bib') as bibtex_file:
     bib_database = bibtexparser.load(bibtex_file)
 
-for entry in bib_database.entries:
-    if "ID" in entry and "author" in entry and "title" in entry:
-        if entry["ID"][:6] == "EPRINT" and "note" in entry and entry["note"][:5] == "\\url{":
-            url = entry["note"][5:-1]  # grab the url of the eprint
-            print(entry["ID"], entry["author"].replace("\n", " "),
-                    entry["title"] + " % " + url, sep="; ")
-        else:
-            print(entry["ID"], entry["author"].replace("\n", " "),
-                    entry["title"], sep="; ")
+entry_list = bib_database.entries
+
+entry_list.sort(key=lambda entry:
+    (-int(entry["year"]) if "year" in entry else 0,
+    entry["author"] if "author" in entry else "ZZZ",
+    entry["title"] if "title" in entry else "ZZZ",
+    entry["ID"].replace("EPRINT", "ZEPRINT") if "ID" in entry else "ZZZ"))
+# Sort by year (more recent first) then journal ID and authors.
+# If ambiguity, put EPRINT entries last.
+
+for entry in entry_list:
+    print(entry["ID"] if "ID" in entry else "NO ID", end="; ")
+    print(entry["author"].replace("\n", " ") if "author" in entry else "NO AUTHOR", end="; ")
+    print(entry["title"] if "title" in entry else "NO TITLE", end="")
+    if entry["ID"][:6] == "EPRINT" and "note" in entry and entry["note"][:5] == "\\url{":
+        url = entry["note"][5:-1]  # grab the url of the eprint
+        print(" %", url, end="")
+    print()  # New line
